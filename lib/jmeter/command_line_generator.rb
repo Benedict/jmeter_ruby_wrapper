1a5637bac1e05f59abce126314354e163d997fa1
Aldric Giacomoni <trevoke@gmail.com>
2012-02-13 11:49:39 -0500

    Add a main_log method for the jmeter log

--- lib/jmeter/command_line_generator.rb
+++ lib/jmeter/command_line_generator.rb
@@ -19,8 +19,12 @@ module Jmeter
       "-l#{@guid}_#{@profile}.log "
     end
 
+    def main_log
+      "-j#{@guid}_jmeter.log "
+    end
+
     #Yep, this isn't tested, because it's just standard Ruby.
-    def random_available_local_port
+    def local_port
       socket = Socket.new(:INET, :STREAM, 0)
       socket.bind(Addrinfo.tcp("127.0.0.1", 0))
       port = socket.local_address.ip_port
--- test/unit/command_line_generator_test.rb
+++ test/unit/command_line_generator_test.rb
@@ -2,19 +2,27 @@ require "test_helper"
 
 class CommandLineGeneratorTest < Test::Unit::TestCase
 
+  GUID = 4
+  PROFILE = "AnnoyingUser"
+
   def test_the_command_line_has_nongui_option
     clg = Jmeter::CommandLineGenerator.new
     assert_equal "jmeter -n", clg.base_command
   end
 
   def test_accepts_the_name_of_a_profile
-    clg = Jmeter::CommandLineGenerator.new :profile => "AnnoyingUser"
-    assert_equal "-tAnnoyingUser.jmx ", clg.profile
+    clg = Jmeter::CommandLineGenerator.new :profile => PROFILE
+    assert_equal "-t#{PROFILE}.jmx ", clg.profile
   end
 
   def test_chooses_the_output_file_based_on_unique_identifier_and_profile_name
-    clg = Jmeter::CommandLineGenerator.new :profile => "AnnoyingUser", :guid => 4
-    assert_equal "-l4_AnnoyingUser.log ", clg.output_file
+    clg = Jmeter::CommandLineGenerator.new :profile => PROFILE, :guid => GUID
+    assert_equal "-l#{GUID}_#{PROFILE}.log ", clg.output_file
+  end
+
+  def test_chooses_a_log_file_with_guid_for_the_main_log
+    clg = Jmeter::CommandLineGenerator.new :profile => PROFILE, :guid => GUID
+    assert_equal "-j#{GUID}_jmeter.log ", clg.main_log
   end
 
 end
